8b007c11ac8f57e0b98f534bfd4fe7f151682f6b
ashwamedh-street <86829156+ashwamedh-street@users.noreply.github.com>
2026-02-01 01:46:26 +0000

    Update README.md

--- client/src/components/ActivityTeam/TableNavigation.js
+++ client/src/components/ActivityTeam/TableNavigation.js
@@ -71,7 +71,9 @@ export default function SimpleTabs(props) {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
+        {/* ************************************************************* */}
         <ActivitiesTotal/>
+        {/* ******************************************************************* */}
       </TabPanel>
     </div>
   );
